c2a8f679207481764f83415c8ebc8e5d1097fc1f
Franco Catena <francocatena@gmail.com>
2013-04-05 20:32:23 -0300

    Sidekiq client config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost', namespace: 'enrollments' }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost', namespace: 'enrollments' }
+end
