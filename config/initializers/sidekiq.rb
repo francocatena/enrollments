15648c437346f90063e929a32c2788a646abbbe2
Franco Catena <francocatena@gmail.com>
2013-04-05 20:43:37 -0300

    Another sidekiq config modification =)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost', namespace: 'enrollments' }
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'enrollments' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost', namespace: 'enrollments' }
+  config.redis = { url: 'redis://localhost:6379/10', namespace: 'enrollments' }
 end
