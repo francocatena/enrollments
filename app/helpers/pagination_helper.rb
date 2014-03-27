38d04c554a8daa01422a412589b2762db57363e4
Franco Catena <francocatena@gmail.com>
2014-03-27 18:56:14 -0300

    Pagination links in people index =)

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -1,5 +1,5 @@
 module PaginationHelper
-  def pagination_links(objects, params = nil)
+  def pagination_links objects, params = nil
     pagination_links = will_paginate objects,
       inner_window: 1, outer_window: 1, params: params,
       renderer: BootstrapPaginationHelper::LinkRenderer,
@@ -14,21 +14,6 @@ module PaginationHelper
 
     pagination_links ||= ''.html_safe
 
-    content_tag(:div, pagination_links + page_entries, class: 'pagination-container')
-  end
-
-  def empty_pagination_links
-    previous_tag = content_tag(
-      :li,
-      content_tag(:a, t('will_paginate.previous_label').html_safe),
-      class: 'previous disabled'
-    )
-    next_tag = content_tag(
-      :li,
-      content_tag(:a, t('will_paginate.next_label').html_safe),
-      class: 'next disabled'
-    )
-
-    content_tag(:ul, previous_tag + next_tag, class: 'pager pull-right')
+    content_tag :div, pagination_links + page_entries, class: 'pagination-container'
   end
 end
